c18395e955e449d2f2955b8208187f093fe059b5
abhinav-9126 <abhinavrawat217@gmail.com>
2026-06-20 09:11:52 +0530

    solution

---
+++ SequenceGame.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class SequenceGame
+{
+    public static void main(String[] args) {
+    Scanner sc= new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] b = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                b[i] = sc.nextInt();
+            }
+
+            ArrayList<Integer> ans = new ArrayList<>();
+
+            ans.add(b[0]);
+
+            for (int i = 1; i < n; i++) {
+                if (b[i - 1] > b[i]) {
+                    ans.add(1);
+                }
+                ans.add(b[i]);
+            }
+
+            System.out.println(ans.size());
+
+            for (int x : ans) {
+                System.out.print(x + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
